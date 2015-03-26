08764b328674aa249caab7715efd8b774a734543
kd5fcy <kd5fcy@gmail.com>
2015-03-26 06:13:20 -0700

    added if statements to any php that wrote html to check for user_id

--- application/views/users.php
+++ application/views/users.php
@@ -5,7 +5,7 @@
 	<?php $book_reviews = $this->Bookdb->book_reviews(); ?>
 	<?php $book_count = $this->Bookdb->book_count(); ?>
 	<meta charset="utf-8">
-	<title><?php echo $user['first_name'] . " " . $user['last_name']; ?>'s Reviews</title>
+	<title><?php if($this->session->userdata('user_id')){echo $user['first_name'] . " " . $user['last_name'];} ?>'s Reviews</title>
 	<style type="text/css"></style>
 	<script src="<?php echo base_url(); ?>assets/js/jquery-2.1.3.min.js" type="text/javascript"></script>
 	<script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
@@ -23,20 +23,22 @@
 	</nav>
 	<div class="panel panel-default" id='content'>
 		<div class='panel-body'>
-			<h3>User Alias: <?php echo $user['alias']; ?></h3>
-			<h4>Name: <?php echo $user['first_name'] . " " . $user['last_name']; ?></h4>
-			<h4>Email: <?php echo $user['email']; ?></h4>
-			<h4>Total Reviews: <?php echo $book_count; ?></h4>
+			<h3>User Alias: <?php if($this->session->userdata('user_id')){echo $user['alias'];} ?></h3>
+			<h4>Name: <?php if($this->session->userdata('user_id')){echo $user['first_name'] . " " . $user['last_name'];} ?></h4>
+			<h4>Email: <?php if($this->session->userdata('user_id')){echo $user['email'];} ?></h4>
+			<h4>Total Reviews: <?php if($this->session->userdata('user_id')){echo $book_count;} ?></h4>
 		</div>
 		<div class='panel-body'>
 			<h4>Posted Reviews on the following books:</h4>
 			<ul>
 				<?php
+				if($this->session->userdata('user_id')){
 					foreach ($book_reviews as $key) {
 						foreach ($key as $id => $name) {
 							echo "<li><a href='/books/book/" . $id . "'>" . $name . "</a></li>";
 						}
 					}
+				}
 				?>
 			</ul>
 		</div>
